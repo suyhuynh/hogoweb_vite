50930d52fe49582db2314923c548db6bec6578df
suyhuynh <126637003+suyhuynh@users.noreply.github.com>
2023-11-28 15:18:36 +0700

    Fixbug company

---
+++ public/web/img/location.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-type: image/svg+xml');
+?>
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 58 58" style="enable-background:new 0 0 512 512" xml:space="preserve" class="">
+	<g>
+		<g xmlns="http://www.w3.org/2000/svg" id="Page-1" fill="none" fill-rule="evenodd"><g id="072---Location">
+			<path id="Path" d="m27.358.045c-10.9567542.62301362-20.62321463 7.37762774-24.97556939 17.4521311-4.35235476 10.0745033-2.64491578 21.7428331 4.41156939 30.1478689.30257852.3524064.32043995.8674801.043 1.24l-5.637 7.515c-.22726323.3030176-.2638192.7084296-.09442719 1.0472136s.51565513.5527864.89442719.5527864h27c10.5500691-.0099345 20.262462-5.7485508 25.3611413-14.9847581s4.7791889-20.5127414-.8342581-29.4454493c-5.6134469-8.93270779-15.6351756-14.11221054-26.1688832-13.5247926z" fill="#<?php echo $_GET['fill'] ?? '4eb92d'; ?>" data-original="#9fc9d3" class=""/>
+			<path id="Path" d="m46 22c0 17-17 31-17 31s-17-14-17-31c0-9.3888407 7.6111593-17 17-17s17 7.6111593 17 17z" fill="#ffffff" data-original="#e64c3c" style="" class=""/>
+			<circle id="Oval" cx="29" cy="22" fill="#<?php echo $_GET['fill'] ?? '4eb92d'; ?>" r="8" data-original="#ecf0f1" style="" class=""/>
+		</g>
+	</g>
+</g>
+</svg>
