45249d520eed9307dfa4bb3368ee9819d58bc742
suyhuynh <126637003+suyhuynh@users.noreply.github.com>
2023-11-28 17:03:10 +0700

    Fixbug

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import inject from '@rollup/plugin-inject';
 
 export default defineConfig({
     base: '',
     plugins: [
+        inject({
+            jQuery: "jquery",
+            require: "require",
+            "window.jQuery": "jquery",
+            $: "jquery",
+            "window.moment": "moment"
+        }),
         laravel({
             input: ['resources/assets/backend/scss/app.scss', 'resources/assets/backend/app.js'],
             refresh: true,
