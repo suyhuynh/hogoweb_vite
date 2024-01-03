6d5ab6c6e55ac32e1d3b885d658448ccc0c8dbdd
suyhuynh <126637003+suyhuynh@users.noreply.github.com>
2024-01-03 17:10:02 +0700

    Fixbug main

--- resources/assets/backend/app.js
+++ resources/assets/backend/app.js
@@ -1,33 +1,29 @@
-import jquery from 'jquery'; './node_modules/jquery';
-window.jquery = jquery;
+import './node_modules/jquery';
 
 import './node_modules/vue';
 import './node_modules/lodash';
-import moment from './node_modules/moment';
+import './node_modules/moment';
 window.moment = moment;
-// import picker from './node_modules/picker';
-// window.picker = picker;
 
+import './node_modules/picker';
 import './node_modules/daterangepicker/daterangepicker.js';
-
-
 import './node_modules/select2';
 import './node_modules/tinymce';
 import './node_modules/jquery.uniform';
 
-// import './admin/app/js/plugins/forms/styling/uniform.min.js';
-// import './admin/app/js/plugins/forms/styling/switchery.min.js';
-// import './admin/app/js/plugins/forms/selects/bootstrap_multiselect.js';
-// import './admin/app/js/main/bootstrap.bundle.min.js';
-// import './admin/app/js/plugins/loaders/blockui.min.js';
-// import './admin/app/js/plugins/editors/ace/ace.js';
-// import './admin/app/cropper/cropper.min.js';
-// import './admin/app/js/plugins/notifications/bootstrap-notify.js';
-// import './admin/app/js/plugins/jquery-confirm/jquery-confirm.min.js';
-// import './admin/assets/vuejs/color/vue-color.min.js';
+import './admin/app/js/plugins/forms/styling/uniform.min.js';
+import './admin/app/js/plugins/forms/styling/switchery.min.js';
+import './admin/app/js/plugins/forms/selects/bootstrap_multiselect.js';
+import './admin/app/js/main/bootstrap.bundle.min.js';
+import './admin/app/js/plugins/loaders/blockui.min.js';
+import './admin/app/js/plugins/editors/ace/ace.js';
+import './admin/app/cropper/cropper.min.js';
+import './admin/app/js/plugins/notifications/bootstrap-notify.js';
+import './admin/app/js/plugins/jquery-confirm/jquery-confirm.min.js';
+import './admin/assets/vuejs/color/vue-color.min.js';
 
-// import './admin/assets/js/app.js';
-// import './admin/assets/vuejs/components.js';
-// import './admin/assets/vuejs/component_forms.js';
-// import './admin/assets/vuejs/helper.js';
-// import './admin/app/js/demo_pages/login.js';
+import './admin/assets/js/app.js';
+import './admin/assets/vuejs/components.js';
+import './admin/assets/vuejs/component_forms.js';
+import './admin/assets/vuejs/helper.js';
+import './admin/app/js/demo_pages/login.js';
